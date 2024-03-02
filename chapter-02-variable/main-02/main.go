6c9af33723a8c4f684a9c7ddcc46188b8ca5ce33
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-02 10:23:54 +0100

    a

--- chapter-02-variable/main-02/main.go
+++ chapter-02-variable/main-02/main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var smsSendingLimit int
-	var costPerSMS float64
-	var hasPermission bool
-	var username string
+	// Same as below - start
+	// var congrats string
+	// congrats = "Congratulations!"
+	// Same as below - end
 
-	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+	congrats := "Congratulations!"
+
+	fmt.Println(congrats)
 }
