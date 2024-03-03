67878e068aea360ce197fd54e8824800ec3dab8a
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-03 22:25:09 +0100

    Apply mystrings.Reverse

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/xiaokatech/go-lab
 
 go 1.22.0
+
+replace github.com/xiaokatech/go-lab-package v0.0.0 => ../go-lab-package
+
+require (
+    github.com/xiaokatech/go-lab-package v0.0.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/xiaokatech/go-lab-package/mystrings"
+)
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(
+		mystrings.Reverse("hello world"),
+	)
 }
